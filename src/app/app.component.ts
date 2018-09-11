7fdf445424ee4f542f9e56ce8e4259920f5f923e
viet <viettangle12@gmail.com>
2018-09-10 23:23:54 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'parentapp';
+
+export class AppComponent implements OnInit {
+  title = 'parent app';
+  message = '';
+  messageFromChild = '';
+
+  ngOnInit(): void {
+    if (window.addEventListener) {
+      window.addEventListener('message', (event) => {
+        const dataFromChildIframe = event.data;
+        console.log(dataFromChildIframe);
+        this.messageFromChild = dataFromChildIframe.messageFromParent;
+      });
+    }
+
+  }
+
+  submit = () => {
+    console.log(this.message);
+    const destination = document.getElementById('destination');
+    // destination.postMessage(data, 'http://localhost:4200/');
+  }
 }
